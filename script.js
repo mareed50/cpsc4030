ce26720887971452768cd1a2b8247eb8f98abdfc
Michael Reed <113122576+mareed50@users.noreply.github.com>
2023-12-04 21:36:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const ccolorScale = d3.scaleLinear()
     .range(["#E0F8FF", "#000080"]);
 const colorScale = d3.scaleLinear()
     .domain([0, maxCount])
-    .range(["#fff3e0", "#e65100"]);
+    .range(["#ffb74d", "#e65100"]);
     
 const attributes = [
     "NUMBER OF PERSONS INJURED",
